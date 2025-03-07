dc8084a5fc35cba2bf5ca1a7138454e7ccfba4b9
ErickHengker <nholdyrealscout10133@gmail.com>
2025-03-07 19:13:48 +0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        //Database db = new Database();
+       // db.open();
+       // System.out.println(db.update());
+       // db.view();
+
+    }
+}
